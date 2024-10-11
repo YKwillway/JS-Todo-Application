06f84d09dc04ac078a210589dc6068534ca0dac2
YKwillway <yuchan0708bicky@gmail.com>
2024-10-12 06:50:58 +0900

    var, const, letの使い方について

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log('hoge');
+// var -> 上書き可, 再宣言可
+var val1 = 'var変数';
+console.log(val1);
+
+val1 = 'ababa';
+console.log(val1);
+
+var val1 = 'bahaha';
+console.log(val1);
+
+// let -> 上書き可, 再宣言不可
+let val2 = 'let変数';
+console.log(val2);
+
+val2 = 'uhaha';
+console.log(val2);
+
+// const -> 上書き不可, 再宣言不可
+const val3 = 'const変数';
+console.log(val3);
+
+const val4 = {
+  name: '川端雄介',
+  age: 29,
+};
+console.log(val4);
+
+val4.name = 'Yusuke Kawabata';
+val4.address = 'Chiba';
+console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ['dog', 'cat'];
+console.log(val5);
+
+val5[0] = 'bird';
+console.log(val5);
+
+val5[4] = 'gorilla';
+console.log(val5);
+
+val5[2] = 'geraff';
+console.log(val5);
